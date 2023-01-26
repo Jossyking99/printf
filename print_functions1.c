ee857d3e9d270f06a779b0ad4c5762bee87564c9
cinyokpoko <96532962+cinyokpoko@users.noreply.github.com>
2023-01-26 11:35:44 +0100

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -3,6 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
+ * by Joseph and Ciny
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
