a57833cb65cfe91559b6a4a6b2b2a6ae74d3cf2a
cinyokpoko <96532962+cinyokpoko@users.noreply.github.com>
2023-01-26 11:14:04 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * by Joseph and Ciny
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
