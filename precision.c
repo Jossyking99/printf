d3e2ca87559b92b7e34498af8c8276c2ec2781a5
cinyokpoko <96532962+cinyokpoko@users.noreply.github.com>
2023-01-26 11:34:07 +0100

    Update precision.c

--- precision.c
+++ precision.c
@@ -2,6 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
+ * by Joseph and Ciny
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
