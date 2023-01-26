2ce71e18ce14b977638824257bcb6862bab4da7b
cinyokpoko <96532962+cinyokpoko@users.noreply.github.com>
2023-01-26 11:36:57 +0100

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -3,6 +3,7 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
+ * by Joseph and Ciny
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
