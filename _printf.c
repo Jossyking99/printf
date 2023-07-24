a877dad8cf6c728c63303c398611d794c9757bbb
Joe-Web <95709035+Jossyking99@users.noreply.github.com>
2023-07-24 07:19:52 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * by Joseph and Ciny
+ * by Joseph and Ciny B
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
